545c0dec3cdcf9cbcf15a7be1c6aa1ccfabbcc3a
v0agent <it+v0agent@vercel.com>
2026-06-05 22:25:48 +0000

    feat: add PresentationLauncher and new presentation page
    Added PresentationLauncher component and created new presentation page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { SectionAib } from '@/components/study/section-aib'
 import { SectionRiba } from '@/components/study/section-riba'
 import { SectionRahn } from '@/components/study/section-rahn'
 import { DownloadPdf } from '@/components/study/download-pdf'
+import { PresentationLauncher } from '@/components/study/presentation'
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
       className="mx-auto min-h-screen w-full max-w-4xl px-4 py-8 md:px-6 md:py-12"
     >
       <DownloadPdf />
+      <PresentationLauncher />
 
       <Cover />
       <TableOfContents />
